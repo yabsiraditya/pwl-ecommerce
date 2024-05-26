c00270e820391fb372a0ae3dbe8387fa312da3d4
yabsiraditya <yabsir.aditya@gmail.com>
2024-05-26 20:15:55 +0700

    update sign in sing up home

--- index.php
+++ index.php
@@ -7,68 +7,168 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="src/css/style.css">
-  <title>Toko Roti </title>
+  <title>Home | Toko Roti Alta Bakery</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="css/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
   <!-- Navbar -->
-  <header>
-    <div class="navbar">
-      <ul class="nav">
-        <li class="item">
-            <a href="index.html" class="dropbtn"><i data-count="3" class="fa-md fa-solid fa-cart-shopping"></i> Cart  <span class="badge">0</span></a>
-        </li>
-        <li class="item">
-          <div class="dropdown">
-            <a class="nav-link dropbtn" href="<?php echo isset($_SESSION['user']) && $_SESSION['user'] ? '#' : 'login.php' ?>"></i> Account</a>
-            <div class="dropdown-content">
-              <a href="#" class="nav-link">Profile</a>
-              <a href="#" class="nav-link">Dashboard</a>
-              <a href="logout.php" class="nav-link">Logout</a>
-            </div>
-          </div>
-        </li>
-      </ul>
-
-      <div class="logo">
-        <img src="https://source.unsplash.com/random/20%C3%9720/?fruit">
+  <nav class="navbar fixed-top navbar-expand-lg shadow-sm" style="background-color: palegoldenrod;">
+    <div class="container">
+      <a class="navbar-brand fw-bold" style="color: #C15440;" href="#">
+        <img src="img/logo.png" alt="Logo" width="50" class="d-inline-block align-text-center me-2">
+        ALTA BAKERY
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link fw-medium" href="#"><i class="fa-solid fa-cart-shopping"></i> Cart <span class="badge rounded-circle text-bg-danger">0</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropbtn fw-medium" href="<?php echo isset($_SESSION['user']) && $_SESSION['user'] ? '#' : 'login.php' ?>">
+            <i class="fa-solid fa-user"></i> Account
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item fw-medium" href="#">Profile</a></li>
+              <li><a class="dropdown-item fw-medium" href="#">Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item fw-medium" href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
-  </header>
+  </nav>
   
   <!-- Slideshow  -->
-  <section>
-  <div class="slideshow-container">
-    <div class="mySlides fade">
-      <img src="https://source.unsplash.com/random/?Bread&1" style="width:100%; height: 600px; object-fit: cover;">
-    </div>
-    <div class="mySlides fade">
-      <img src="https://source.unsplash.com/random/?Bread&2" style="width:100%; height: 600px; object-fit: cover;">
-    </div>
-    <div class="mySlides fade">
-      <img src="https://source.unsplash.com/random/?Bread&3" style="width:100%; height: 600px; object-fit: cover;">
-    </div>
-
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  <div class="container">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <img src="img/logo.png" class="d-block w-100 slider" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="img/logo.png" class="d-block w-100 slider" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/logo.png" class="d-block w-100 slider" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div> 
   </div>
-
-  <div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-  </div>
-  </section>
 
   <!-- Search Bar -->
-  <div class="search-bar">
-    <form id="searchthis" action="#" method="get">
-      <input class="search-box" name="search" type="text" placeholder="Search"/>
-      <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
-    </form>
+  <div class="d-flex justify-content-center align-items-center"> 
+    <div class="container w-50 input-group mb-4 mt-5">
+      <input type="text" class="form-control" placeholder="Search" aria-describedby="button-addon2">
+      <button class="btn btn-primary" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </div>
+  </div>
+  <!-- Product -->
+  <div class="container">
+    <div class="row d-flex justify-content-start align-items-center">
+      <div class="col-6 col-md-4 mt-3">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 mt-3">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 mt-3">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 mt-3">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-md-4 mt-3">
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Pagination -->
+    <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center mt-3">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item active"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+    </nav>
   </div>
 
+  <!-- Footer -->
+  <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <img class="me-2" src="img/logo.png" width="50">
+      </a>
+      <span class="mb-3 mb-md-0 fw-medium">© 2024 Alta Bakery, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-brands fa-twitter fa-2xl"></i></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-brands fa-instagram fa-2xl"></i></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fa-brands fa-facebook fa-2xl"></i></use></svg></a></li>
+    </ul>
+  </footer>
+  </div>
 </body>
 <script src="https://kit.fontawesome.com/47dcae39d3.js" crossorigin="anonymous"></script>
-<script src="src/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="js/script.js"></script>
 </html>
